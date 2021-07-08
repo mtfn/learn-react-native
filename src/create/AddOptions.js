107f3c693ce5304b7fd53e173f660c5bad226094
forkpoweroutlet <48189055+forkpoweroutlet@users.noreply.github.com>
2021-07-08 13:42:44 -0700

    Creation interface prototype

--- src/create/AddOptions.js
+++ src/create/AddOptions.js
@@ -1,12 +1,58 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, TouchableOpacity, Text, FlatList, KeyboardAvoidingView, TextInput } from 'react-native';
+import uuid from 'react-native-uuid';
 
 import styles from './styles/AddOptions.style'
-  
+
 export default function AddOptions() {
+    const [ data, setData ] = useState([]);
+    const [ text, setText ] = useState('');
+
+    const renderItem = ({ item }) => (
+        <TouchableOpacity onPress={() => {
+            setData(prev => prev.filter(x => x.id !== item.id));
+        }}>
+            <View style={styles.item}>
+                <Text style={styles.title}>{item.text}</Text>
+            </View>
+        </TouchableOpacity>
+    );
+
+    const textInput = useRef(null);
+    const onSubmit = () => {
+        const newItem = {
+            text: text,
+            id: uuid.v4()
+        }
+        setData(prev => [...prev, newItem]);
+        textInput.current.clear();
+    }
 
     return (
-    <View style={styles.container}>
-        <Text style={styles.question}>Time to make your poll:</Text>
-    </View>);
+        <KeyboardAvoidingView
+            behavior={Platform.select({ ios: 'padding' })}
+            style={styles.keyboardAvoidingView}
+        >
+            
+            <TouchableOpacity disabled={data.length < 2} style={data.length > 1 ? styles.submit : styles.submitDisabled}>
+                <Text style={styles.submitText}>Done</Text>
+            </TouchableOpacity>
+            
+            <FlatList
+                style={styles.list}
+                contentContainerStyle={{}} 
+                data={data}
+                extraData={data}
+                renderItem={renderItem}
+            />
+
+            <TextInput
+                style={styles.input}
+                ref={textInput}
+                onChangeText={setText}
+                onSubmitEditing={onSubmit}
+            />
+
+        </KeyboardAvoidingView>
+    );
 }
--- src/create/styles/AddOptions.style.js
+++ src/create/styles/AddOptions.style.js
@@ -1,6 +1,58 @@
 import { StyleSheet } from 'react-native';
-import header from './common/header';
 
-export default StyleSheet.create(Object.assign({
-    
-}, header));
+const submit = {
+    borderWidth: 1,
+    marginLeft: '25%',
+    marginRight: '25%',
+    padding: '1%',
+    borderColor: 'limegreen',
+    width: '50%',
+    marginBottom: '10%'
+}
+
+export default StyleSheet.create({
+    item: {
+        borderColor: '#d5d9e0',
+        borderWidth: 1,
+        borderRadius: 5,
+        padding: '3%',
+        marginVertical: '2%',
+        marginHorizontal: '2%',
+    },
+
+    title: {
+        color: '#d5d9e0',
+        fontSize: 12,
+    },
+
+    input: {
+        height: 48,
+        margin: '2%',
+        borderWidth: 1,
+        borderColor: '#d5d9e0',
+        borderRadius: 10,
+        color: '#d5d9e0',
+        paddingLeft: '2%',
+        paddingRight: '2%',
+        marginTop: '5%'
+    },
+
+    keyboardAvoidingView: {
+        marginTop: '5%',
+        justifyContent: 'flex-end'
+    },
+
+    list: {
+        height: '80%'
+    },
+
+    submit: submit,
+
+    submitDisabled: Object.assign({}, submit, {borderColor: 'lightcoral'}),
+
+    submitText: {
+        fontSize: 30,
+        textAlign: 'center',
+        color: '#d5d9e0'
+    }
+});
