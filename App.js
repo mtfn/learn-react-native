d4a20dde31bf7a9e9447906afefd75ff91422c33
forkpoweroutlet <48189055+forkpoweroutlet@users.noreply.github.com>
2022-07-30 14:24:06 -0700

    Register new screens

--- App.js
+++ App.js
@@ -6,8 +6,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 import MainView from './src/main/MainView';
 import SelectPollType from './src/create/SelectPollType';
 import AddOptions from './src/create/AddOptions';
+import OtherOptions from './src/create/OtherOptions';
 
 import Plurality from './src/vote/Plurality';
+import Ranked from './src/vote/Ranked';
 
 const hOpts = { headerTintColor: '#d5d9e0' }
 
@@ -19,7 +21,11 @@ export default function App() {
         <Stack.Screen name="Home" component={MainView} options={{ headerShown: false }}/>
         <Stack.Screen name="Poll type" component={SelectPollType} options={hOpts} />
         <Stack.Screen name="Create poll" component={AddOptions} options={hOpts} />
+        <Stack.Screen name="Other options" component={OtherOptions} options={hOpts} />
+
         <Stack.Screen name="Plurality poll" component={Plurality} options={hOpts} />
+        <Stack.Screen name="Ranked poll" component={Ranked} options={hOpts} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
